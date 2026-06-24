c648e4d4a6546fd0a23c9ab6f5ff76637e3acd91
ArshilKhan09 <arshilkhan529@gmail.com>
2026-06-24 19:41:47 +0530

    Updated Str1.java

--- src/LearnString/Str1.java
+++ src/LearnString/Str1.java
@@ -5,10 +5,20 @@ public class Str1 {
 
         // Both point one object ->"Arshil" in String pool
         String name = "Arshil";
-        String same = "Arshil";
+        String sameName = "Arshil";
 
         // It is created somewhere else in Heap Memory whether the name "Arshil" is same
         String newName = new String("Arshil");
 
+
+        if(name == sameName){       // OP-> Same
+            System.out.println("Both are Same");
+        }
+        if(name == newName){        // OP-> Not Same
+            System.out.println("Both are Same");
+        }
+        else{
+            System.out.println("Both are not Same");
+        }
     }
 }
