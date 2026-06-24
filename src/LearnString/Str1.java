fcba3bf0150488480f174609bc2b1ee053490304
ArshilKhan09 <arshilkhan529@gmail.com>
2026-06-24 18:54:36 +0530

    Understand about how String Store in Stack and Heap

--- src/LearnString/Str1.java
+++ src/LearnString/Str1.java
@@ -1,4 +1,14 @@
 package LearnString;
 
 public class Str1 {
+    public static void main(String[] args) {
+
+        // Both point one object ->"Arshil" in String pool
+        String name = "Arshil";
+        String same = "Arshil";
+
+        // It is created somewhere else in Heap Memory whether the name "Arshil" is same
+        String newName = new String("Arshil");
+
+    }
 }
